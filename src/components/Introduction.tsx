e6523e600a6f9bb23d67eeef77b5c24e71db91b3
Nier704 <lorecode12@gmail.com>
2024-06-05 03:00:10 +0000

    add icon title

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -33,7 +33,8 @@ const Me = () => {
          <img
             className='w-[187px] rounded-[360px]'
             src={icon} 
-            alt="icon" 
+            alt="icon"
+            title='pau no cu de quem ta lendo 😉'
          />
          <div className='flex flex-col gap-3 ml-2'>
             <h3 className='text-6xl font-[500]'>Lore 有線</h3>
